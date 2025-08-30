3aaeb45fd628da9b939f15197cd6a315ee6b1eea
Yaman Jain <yamanjain@gmail.com>
2025-08-30 15:40:04 +0530

    Headless

--- get_playwright_page.py
+++ get_playwright_page.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Playwright
 
 
 def get_playwright_page(p: Playwright, matchers, slow_mo=0):
-    do_headless = False
+    do_headless = True
     browser = p.chromium.launch(headless=do_headless, chromium_sandbox=False)
     context = browser.new_context(accept_downloads=True)
     page = context.new_page()
